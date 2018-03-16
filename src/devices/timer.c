881481595ca86ad497b3c449ff1a316045f29c24
Celso <asura254@kaist.ac.kr>
2018-03-16 06:09:24 -0700

    CHANGE THE TEST FILE ALARM-PRIORITY AND THREADS

--- src/devices/timer.c
+++ src/devices/timer.c
@@ -93,7 +93,8 @@ timer_sleep (int64_t ticks)
 
   ASSERT (intr_get_level () == INTR_ON);
   while (timer_elapsed (start) < ticks) 
-    thread_yield ();
+    printf("so this is how this works");
+	thread_yield ();
 }
 
 /* Sleeps for approximately MS milliseconds.  Interrupts must be
